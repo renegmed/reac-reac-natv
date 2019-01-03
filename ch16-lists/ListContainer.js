3944d69845aa74a7880da9f6ad462abc84e0459f
rene <renegmed@yahoo.com>
2019-01-03 11:45:56 -0500

    ch16 - fetching list data (not working)

--- ch16-lists/ListContainer.js
+++ ch16-lists/ListContainer.js
@@ -1,71 +1,61 @@
 import React, { Component } from 'react';
 
+import { fetchItems } from './api';
 import List from './List';
 
 const mapItems = items =>
   items.map((value, i) => ({ key: i.toString(), value }));
 
-// Performs sorting and filtering on the given "data".
-const filterAndSort = (data, text, asc) =>
-  data
-    .filter(
-      i =>
-        // Items that include the filter "text" are returned.
-        // Unless the "text" argument is an empty string,
-        // then everything is included.
-        text.length === 0 || i.includes(text)
-    )
-    .sort(
-      // Sorts either ascending or descending based on "asc".
-      asc
-        ? (a, b) => (b > a ? -1 : a === b ? 0 : 1)
-        : (a, b) => (a > b ? -1 : a === b ? 0 : 1)
-    );
-
 class ListContainer extends Component {
+  // The "source" state is empty because we need
+  // to fetch the data from the API.
   state = {
-    data: filterAndSort(
-      new Array(100).fill(null).map((v, i) => `Item ${i}`),
-      '',
-      true
-    ),
     asc: true,
-    filter: ''
+    filter: '',
+    data: []
   };
+
+  // When the component is first mounted, fetch the initial
+  // items from the API, then
+  componentDidMount() {
+    fetchItems(this.state.filter, this.state.asc)
+      .then(resp => resp.json())
+      .then(({ items }) => {
+        this.setState({ data: mapItems(items) });
+      });
+  }
 
   render() {
     return (
       <List
-        data={mapItems(this.state.data)}
+        data={this.state.data}
         asc={this.state.asc}
         onFilter={text => {
-          // Updates the "filter" state, the actualy filter text,
-          // and the "source" of the list. The "data" state is
-          // never actually touched - "filterAndSort()" doesn't
-          // mutate anything.
-          this.setState({
-            filter: text,
-            data: filterAndSort(this.state.data, text, this.state.asc)
-          });
+          // Makes an API call when the filter changes...
+          fetchItems(text, this.state.asc)
+            .then(resp => resp.json())
+            .then(({ items }) =>
+              this.setState({
+                filter: text,
+                data: mapItems(items)
+              })
+            );
         }}
         onSort={() => {
-          this.setState({
-            // Updates the "asc" state in order to change the
-            // order of the list. The same principles as used
-            // in the "onFilter()" handler are applied here,
-            // only with diferent arguments passed to
-            // "filterAndSort()"
-            asc: !this.state.asc,
-            data: filterAndSort(
-              this.state.data,
-              this.state.filter,
-              !this.state.asc
-            )
-          });
+          // Makes an API call when the sort order changes...
+          fetchItems(this.state.filter, !this.state.asc)
+            .then(resp => resp.json())
+            .then(({ items }) =>
+              this.setState({
+                asc: !this.state.asc,
+                data: mapItems(items)
+              })
+            );
         }}
       />
     );
   }
 }
+
 
 export default ListContainer;
