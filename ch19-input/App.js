dac19e0a77690af5f9ca5cd1032467b3210e4f94
rene <renegmed@yahoo.com>
2019-01-03 09:40:03 -0500

    ch19 - toggling on and off

--- ch19-input/App.js
+++ ch19-input/App.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { fromJS } from 'immutable';
 
 import styles from './styles';
-import Select from './Select';
+import Switch from './Switch';
 
-export default class SelectingOptions extends Component {
-  // The state is a collection of "sizes" and
-  // "garments". At any given time there can be
-  // selected size and garment.
+export default class TogglingOnAndOff extends Component {
   state = {
     data: fromJS({
-      sizes: [
-        { label: '', value: null },
-        { label: 'S', value: 'S' },
-        { label: 'M', value: 'M' },
-        { label: 'L', value: 'L' },
-        { label: 'XL', value: 'XL' }
-      ],
-      selectedSize: null,
-      garments: [
-        { label: '', value: null, sizes: ['S', 'M', 'L', 'XL'] },
-        { label: 'Socks', value: 1, sizes: ['S', 'L'] },
-        { label: 'Shirt', value: 2, sizes: ['M', 'XL'] },
-        { label: 'Pants', value: 3, sizes: ['S', 'L'] },
-        { label: 'Hat', value: 4, sizes: ['M', 'XL'] }
-      ],
-      availableGarments: [],
-      selectedGarment: null,
-      selection: ''
+      first: false,
+      second: false
     })
   };
 
@@ -43,55 +24,31 @@ export default class SelectingOptions extends Component {
   }
 
   render() {
-    const {
-      sizes,
-      selectedSize,
-      availableGarments,
-      selectedGarment,
-      selection
-    } = this.data.toJS();
+    const { first, second } = this.state.data.toJS();
 
-    // Renders two "<Select>" components. The first
-    // one is a "size" selector, and this changes
-    // the available garments to select from.
-    // The second selector changes the "selection"
-    // state to include the selected size
-    // and garment.
     return (
       <View style={styles.container}>
-        <Select
-          label="Size"
-          items={sizes}
-          selectedValue={selectedSize}
-          onValueChange={size => {
-            this.data = this.data
-              .set('selectedSize', size)
-              .set('selectedGarment', null)
-              .set(
-                'availableGarments',
-                this.data
-                  .get('garments')
-                  .filter(i => i.get('sizes').includes(size))
-              );
+        {/* When this switch is turned on, the
+             second switch is disabled. */}
+        <Switch
+          label="Disable Next Switch"
+          value={first}
+          disabled={second}
+          onValueChange={v => {
+            this.data = this.data.set('first', v);
           }}
         />
-        <Select
-          label="Garment"
-          items={availableGarments}
-          selectedValue={selectedGarment}
-          onValueChange={garment => {
-            this.data = this.data.set('selectedGarment', garment).set(
-              'selection',
-              this.data.get('selectedSize') +
-                ' ' +
-                this.data
-                  .get('garments')
-                  .find(i => i.get('value') === garment)
-                  .get('label')
-            );
+
+        {/* When this switch is turned on, the
+             first switch is disabled. */}
+        <Switch
+          label="Disable Previous Switch"
+          value={second}
+          disabled={first}
+          onValueChange={v => {
+            this.data = this.data.set('second', v);
           }}
         />
-        <Text style={styles.selection}>{selection}</Text>
       </View>
     );
   }
--- ch19-input/styles.js
+++ ch19-input/styles.js
@@ -1,41 +1,16 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      backgroundColor: 'ghostwhite' 
-    },
-  
-    // The outter most container, needs a height. 
-    pickerHeight: {
-      height: 300
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'ghostwhite',
+  },
 
-    // The inner container lays out the picker
-    // components and sets the background color.
-    pickerContainer: {
-      flex: 1,
-      flexDirection: 'column',
-      alignItems: 'center',
-      marginTop: 40,
-      backgroundColor: 'white',
-      padding: 6,
-      height: 240
-    },
-
-    picker: {
-      width: 100,
-      backgroundColor: 'white'
-    },
-
-    selection: {
-      width: 200,
-      marginTop: 230,
-      textAlign: 'center'
-    }
+  customSwitch: {
+    alignItems: 'center',
+    margin: 10,
+  },
 });
   
